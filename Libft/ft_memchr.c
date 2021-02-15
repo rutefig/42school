83ad4621ff52188cb9f1f03195306a36692dff32
Rute Figueiredo <rute.figueiredo92@gmail.com>
2021-02-15 17:08:53 +0000

    ft_memchr done

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,19 @@
+void	*ft_memchr(const void *s, int c, unsigned int n)
+{
+	unsigned int	i;
+	int	c_found;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((char *)s)[i] == (char)c)
+		{
+			c_found = 1;
+			break;
+		}
+		i++;
+	}
+	if (c_found)
+		return ((void *)(s + i));
+	return ((void *)0);
+}
--- Libft/main.c
+++ Libft/main.c
@@ -1,16 +1,14 @@
 #include <string.h>
 #include <stdio.h>
 
-void	*ft_memmove(void *dest, const void *src, unsigned int n);
+void	*ft_memchr(const void *s, int c, unsigned int n);
 
 int	main(void)
 {
-	char dest2[20];
 	char src[20] = "Hello World!";
 	char *c2;
 
-	c2 = (char *)ft_memmove(dest2, src, 10);
+	c2 = (char *)ft_memchr(src, 'e', 10);
 	printf("%p\n", c2);
-	printf("%s\n", dest2);
 	return (0);
 }
